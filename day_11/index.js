d50d178ddf6caf3f72ac13f68e725c3868a6f4ad
Windrushfarer <windrushfarer@gmail.com>
2021-12-13 15:03:06 +0100

    day 11 solution part two

--- day_11/index.js
+++ day_11/index.js
@@ -64,4 +64,59 @@ function partOne(steps) {
   return flashes
 }
 
-partOne(100) // ?
+function partTwo() {
+  const grid = readInputFile(path.join(__dirname, 'input.txt'))
+    .split('\n')
+    .map(line => line.split('').map(Number))
+  const height = grid.length
+  const width = grid[0].length
+
+  function flash(y, x) {
+    const neighbours = getNeighbours(y, x)
+      .filter(([row, cell]) => within(row, cell, height, width) && grid[row][cell] !== 0)
+
+    neighbours.forEach(([row, cell]) => {
+      grid[row][cell] = grid[row][cell] + 1
+    })
+
+    neighbours.forEach(([row, cell]) => {
+      if (grid[row][cell] > 9) {
+        grid[row][cell] = 0
+        flash(row, cell)
+      }
+    })
+  }
+
+  function isSimultanious() {
+    return grid.every(row => row.every(cell => cell === 0))
+  }
+
+  let step = 0;
+
+  while (true) {
+    step++
+    grid.forEach(row => {
+      row.forEach((cell, index) => {
+        row[index] = cell + 1;
+      })
+    })
+
+    grid.forEach((row, y) => {
+      row.forEach((cell, x) => {
+        if (row[x] > 9) {
+          row[x] = 0
+          flash(y, x)
+        }
+      })
+    })
+
+    if (isSimultanious()) {
+      break;
+    }
+  }
+
+  return step
+}
+
+partOne()
+partTwo()
