d46c8ad9b5d5e3f8950ede4448085acb18f8bcf5
Windrushfarer <windrushfarer@gmail.com>
2021-12-13 13:37:51 +0100

    day 10 solution part two + some refactors

--- day_10/index.js
+++ day_10/index.js
@@ -9,14 +9,14 @@ const BRACKETS = {
 }
 const OPEN_BRACKETS = Object.keys(BRACKETS)
 
-const POINTS = {
+const WRONG_BRACKET_POINTS = {
   ')': 3,
   ']': 57,
   '}': 1197,
   '>': 25137,
 }
 
-function checkBrackets(input) {
+function getWrongBracket(input) {
   const stack = []
 
   for (const char of input) {
@@ -29,27 +29,73 @@ function checkBrackets(input) {
     }
   }
 
-  return false
+  return ''
 }
 
+
 function partOne() {
-  const lines = readInputFile(path.join(__dirname, 'test.txt')).split('\n')
+  const lines = readInputFile(path.join(__dirname, 'input.txt')).split('\n')
   const wrongBrackets = []
 
   lines.forEach(line => {
-    const wrongBracket = checkBrackets(line)
+    const wrongBracket = getWrongBracket(line)
 
     if (wrongBracket) {
       wrongBrackets.push(wrongBracket)
     }
   })
 
-  return wrongBrackets.reduce((acc, curr) => acc + POINTS[curr], 0)
+  return wrongBrackets.reduce((acc, curr) => acc + WRONG_BRACKET_POINTS[curr], 0)
+}
+
+/**
+ * @param {string} input
+ * @returns {string[]}
+ */
+function getIncompleteBrackets(input) {
+  const stack = []
+
+  for (const char of input) {
+    if (OPEN_BRACKETS.includes(char)) {
+      stack.push(char)
+    } else if (BRACKETS[stack[stack.length - 1]] === char) {
+      stack.pop()
+    }
+  }
+
+  return stack
+}
+
+const INCOMPLETE_BRACKET_POINTS = {
+  ')': 1,
+  ']': 2,
+  '}': 3,
+  '>': 4,
 }
 
 function partTwo() {
+  const lines = readInputFile(path.join(__dirname, 'input.txt'))
+    .split('\n')
+    .filter(line => !getWrongBracket(line))
 
+  const incompleteBrackets = []
+
+  lines.forEach(line => {
+    const brackets = getIncompleteBrackets(line)
+
+    if (brackets.length > 0) {
+      incompleteBrackets.push(brackets)
+    }
+  })
+
+  return incompleteBrackets
+    .map(brackets => brackets.map(bracket => BRACKETS[bracket]).reverse())
+    .map(brackets => brackets.reduce((acc, curr) => {
+      return (acc * 5) + INCOMPLETE_BRACKET_POINTS[curr]
+    }, 0))
+    .sort((a, b) => a - b)
+    .at(Math.floor(incompleteBrackets.length / 2))
 }
 
-partOne() // ?
-// partTwo() // ?
+partOne()
+partTwo()
