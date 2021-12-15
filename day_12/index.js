9da83016d51faea0210f11db3bdd66830be86190
Windrushfarer <windrushfarer@gmail.com>
2021-12-15 21:18:51 +0100

    day 12 part two + refactor

--- day_12/index.js
+++ day_12/index.js
@@ -28,6 +28,12 @@ function parseInput() {
   return graph
 }
 
+function isAllowedVisitOnce(node, currentPath) {
+  const isBigCave = node === node.toUpperCase()
+
+  return isBigCave || !currentPath.includes(node)
+}
+
 function partOne() {
   const graph = parseInput()
   const paths = []
@@ -39,11 +45,7 @@ function partOne() {
     }
 
     graph[node]
-      .filter(next => {
-        const upper = next.toUpperCase()
-
-        return upper === next || !path.includes(next)
-      })
+      .filter(next => isAllowedVisitOnce(next, path))
       .forEach(next => {
         findPaths(next, [...path, next])
       })
@@ -54,4 +56,48 @@ function partOne() {
   return paths.length
 }
 
-partOne() // ?
+function isAllowedVisitTwice(next, currentPath) {
+  if (next === next.toUpperCase()) {
+    return true
+  }
+
+  if (!currentPath.includes(next)) {
+    return true
+  }
+
+  if (next === 'start') {
+    return false
+  }
+
+  const smalls = currentPath.filter(node => node === node.toLowerCase())
+  const allowed = smalls.every(node => {
+    return smalls.filter(x => node === x).length !== 2
+  })
+
+  return allowed
+}
+
+function partTwo() {
+  const graph = parseInput()
+  const paths = []
+
+  function findPaths(node, path) {
+    if (node === 'end') {
+      paths.push(path)
+      return
+    }
+
+    graph[node]
+      .filter(next => isAllowedVisitTwice(next, path))
+      .forEach(next => {
+        findPaths(next, [...path, next])
+      })
+  }
+
+  findPaths('start', ['start'])
+
+  return paths.length
+}
+
+partOne()
+partTwo()
